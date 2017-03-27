71a815734b33f8b024125d97be1b1d735b0314c2
khnam <khnam@jungang.com>
2017-03-27 11:54:31 +0900

    security

---
+++ src/main/java/com/jungang/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.jungang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN")
+			.and()
+			.withUser("user").password("1234").roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/**").hasRole("USER")
+			.and()
+			.formLogin();
+	}
+}
