d82478206ec5a848d7cad8123bcd87678dc655a9
java <java>
2017-03-27 11:45:23 +0900

    SecurityConfig

---
+++ src/main/java/com/jungang/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.jungang.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+//	@Autowired
+//	UserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		/*
+		 * 인증
+		 */
+		auth.inMemoryAuthentication()
+				.withUser("admin").password("1234").roles("ADMIN")
+				.and()
+				.withUser("user").password("1234").roles("USER");
+//		auth.userDetailsService(userDetailsService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*
+		 * 권한
+		 */
+		http.csrf().disable()									//보안관련
+			.authorizeRequests()
+				.antMatchers("/login/**").hasRole("ADMIN")
+//				.antMatchers("/register/**").hasRole("ADMIN")
+				.antMatchers("/**").permitAll()
+			.and()
+			.formLogin()
+			.permitAll();
+		
+	}
+
+}
--- src/main/resources/META-INF/resources/WEB-INF/tiles/main.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/tiles/main.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" trimDirectiveWhitespaces="true"%>
 <div>메인화면</div>
+<h1>test1</h1>
+<h1>test2</h1>
+<h1>test3</h1>
+
 <script type="text/javascript">
 
 </script>
